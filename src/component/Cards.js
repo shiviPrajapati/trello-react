de7f08f91e644acaf170be170022a1a0aa2301dc
shivi <shivi.prajapati.19.1@mountblue.tech>
2022-06-29 16:33:45 +0530

    add Card Functionality

---
+++ src/component/Cards.js
@@ -0,0 +1,98 @@
+import axios from "axios";
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+
+class Cards extends Component {
+
+    state = {
+        cards: [],
+        addCard: true,
+        inputCardTitle: "",
+    }
+
+    componentDidMount = () => {
+        axios.get(`https://api.trello.com/1/lists/${this.props.cardsOfList.id}/cards?key=4707113ae4bb574864bf59a341bf98bf&token=0b1757680dc35a102d1bc0c026e4991bec39da3dd026dd757d5f6a988c5cfde9`)
+            .then((res) => {
+                this.setState({ cards: res.data })
+            })
+    }
+
+    createCard = () => {
+        this.setState({
+            addCard: !this.state.addCard
+        })
+    }
+
+    handleInput = (e) => {
+        this.setState({
+            inputCardTitle: e.target.value
+        })
+    }
+
+    addCard = (id) => {
+        axios.post(`https://api.trello.com/1/cards?name=${this.state.inputCardTitle}&idList=${id}&key=4707113ae4bb574864bf59a341bf98bf&token=0b1757680dc35a102d1bc0c026e4991bec39da3dd026dd757d5f6a988c5cfde9`)
+            .then((res) => {
+                this.setState({
+                    cards: [...this.state.cards, res.data],
+                    addCard: !this.state.addCard,
+                    inputCardTitle: ""
+                })
+            })
+    }
+
+    closeCard = () => {
+        this.setState({
+            addCard: !this.state.addCard
+        })
+    }
+
+    deleteCard = (id) => {
+        axios.delete(`https://api.trello.com/1/cards/${id}?key=4707113ae4bb574864bf59a341bf98bf&token=0b1757680dc35a102d1bc0c026e4991bec39da3dd026dd757d5f6a988c5cfde9`)
+            .then((res) => {
+                this.setState({
+                    cards: this.state.cards.filter((card) => card.id !== id)
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    {
+                        this.state.cards.map((card) => {
+                            return (
+                                <div key={card.id}>
+                                    <div className="cardContainer">
+                                        <div className="cardItem">
+                                            <div className="cardName">{card.name} </div>
+                                            <span className="dltListItemBtn" onClick={() => this.deleteCard(card.id)}> X </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
+                <div>
+                    {
+                        this.state.addCard ? (
+                            <button className="addListItemBtn" onClick={this.createCard}>Add New Item</button>
+                        ) : (
+                            <div className="listItemModule">
+                                <input className="cardTitle" onChange={this.handleInput}></input>
+                                <div>
+                                    <Button onClick={() => this.addCard(this.props.cardsOfList.id)}>Add Item </Button>
+                                    <span className="closeCardModule" onClick={this.closeCard}> X </span>
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Cards
--- src/component/EachBoard.js
+++ src/component/EachBoard.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { Component } from "react";
 import { Button } from "react-bootstrap";
-import { IoClose } from "react-icons/io5";
+import Cards from "./Cards";
 
 class EachBoard extends Component {
 
@@ -10,88 +10,93 @@ class EachBoard extends Component {
         lists: [],
         listAdd: true,
         inputTitle: ""
-      };
+    };
 
-      componentDidMount = () => {
+    componentDidMount = () => {
         return axios.get(
-          `https://api.trello.com/1/boards/${this.props.match.params.boardId}/lists?key=4707113ae4bb574864bf59a341bf98bf&token=0b1757680dc35a102d1bc0c026e4991bec39da3dd026dd757d5f6a988c5cfde9`)
-          .then((res) => this.setState({lists: res.data}))
-    
-        }
+            `https://api.trello.com/1/boards/${this.props.match.params.boardId}/lists?key=4707113ae4bb574864bf59a341bf98bf&token=0b1757680dc35a102d1bc0c026e4991bec39da3dd026dd757d5f6a988c5cfde9`)
+            .then((res) => this.setState({ lists: res.data }))
 
-        deleteList = (id) => {
-            axios.put(`https://api.trello.com/1/lists/${id}/closed?value=true&key=4707113ae4bb574864bf59a341bf98bf&token=0b1757680dc35a102d1bc0c026e4991bec39da3dd026dd757d5f6a988c5cfde9`)
+    }
+
+    deleteList = (id) => {
+        axios.put(`https://api.trello.com/1/lists/${id}/closed?value=true&key=4707113ae4bb574864bf59a341bf98bf&token=0b1757680dc35a102d1bc0c026e4991bec39da3dd026dd757d5f6a988c5cfde9`)
             .then((res) => {
                 this.setState({
                     lists: this.state.lists.filter((list) => list.id !== id)
                 })
             })
-        }
+    }
 
-        createList = () => {
-            this.setState({
-                listAdd: !this.state.listAdd
-            })
+    createList = () => {
+        this.setState({
+            listAdd: !this.state.listAdd
+        })
 
-        }
+    }
 
-        removeList = () => {
-            this.setState({
-                listAdd: !this.state.listAdd
-            })
-        }
+    removeList = () => {
+        this.setState({
+            listAdd: !this.state.listAdd
+        })
+    }
 
-        handleInput = (e) => {
-            this.setState({
-                inputTitle: e.target.value
-            })
-        }
+    handleInput = (e) => {
+        this.setState({
+            inputTitle: e.target.value
+        })
+    }
 
-        addList = (id) => {
-            this.setState({
-                listAdd: !this.state.listAdd
-            })
-            axios.post(`https://api.trello.com/1/lists?name=${this.state.inputTitle}&idBoard=${id}&key=4707113ae4bb574864bf59a341bf98bf&token=0b1757680dc35a102d1bc0c026e4991bec39da3dd026dd757d5f6a988c5cfde9`)
+    addList = (id) => {
+        this.setState({
+            listAdd: !this.state.listAdd
+        })
+        axios.post(`https://api.trello.com/1/lists?name=${this.state.inputTitle}&idBoard=${id}&key=4707113ae4bb574864bf59a341bf98bf&token=0b1757680dc35a102d1bc0c026e4991bec39da3dd026dd757d5f6a988c5cfde9`)
             .then((res) => {
                 this.setState({
-                    list:[...this.state.lists, res.data],
+                    list: [...this.state.lists, res.data],
                     inputTitle: ""
                 })
                 return axios.get(
                     `https://api.trello.com/1/boards/${this.props.match.params.boardId}/lists?key=4707113ae4bb574864bf59a341bf98bf&token=0b1757680dc35a102d1bc0c026e4991bec39da3dd026dd757d5f6a988c5cfde9`)
-                    .then((res) => this.setState({lists: res.data}))
-              
-            })
-            
-        }
+                    .then((res) => this.setState({ lists: res.data }))
 
-    render(){
-        
+            })
+
+    }
+
+    render() {
+
         return (
             <div className="lists d-flex align-items-start">
                 {
                     this.state.lists.map((list) => {
                         return (
-                            <div className="outerListContainer">
-                            <div className="listContainer">
-                            <div className="d-flex justify-content-between" key={list.id}>
-                                <div className="listName">{list.name}</div>
-                                <span className="deleteList" onClick={() => this.deleteList(list.id)}>close</span>
-                            </div></div></div>
+                            <div key={list.id} className="outerListContainer">
+                                <div className="listContainer">
+                                    <div className="d-flex justify-content-between" key={list.id}>
+                                        <div className="listName">{list.name}</div>
+                                        <span className="deleteList" onClick={() => this.deleteList(list.id)}> X </span>
+                                    </div>
+                                    {
+                                        <Cards cardsOfList={list} />
+                                    }
+                                </div>
+                            </div>
                         )
                     })
                 }
                 {
                     this.state.listAdd ? (
                         <div>
-                            <button className="createListBtn" style={{color:"gray"}} onClick={this.createList}>Add New List</button>
+                            <button className="createListBtn" style={{ color: "gray" }} onClick={this.createList}>Add New List</button>
                         </div>
-                    ):(
+                    ) : (
                         <div className="listModule">
                             <input className="addListInput" type="text" onChange={this.handleInput} value={this.state.inputTitle} placeholder="Enter title"></input>
                             <div className="addListDiv">
-                                <Button  onClick = {() => this.addList(this.props.match.params.boardId)}>Add List</Button> 
-                                <span onClick={this.removeList}><IoClose></IoClose></span>
+                                <Button onClick={() => this.addList(this.props.match.params.boardId)}>Add List</Button>
+                                <span onClick={this.removeList}> X </span>
                             </div>
                         </div>
                     )
